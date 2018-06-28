48e3df0b8db669694eadf446a427c80414d2b049
Lamine Kaba <lkaba@student.42.us.org>
2018-06-28 09:33:10 -0700

    adding fn free dynamique array

--- libft/ft_dynamic_array.c
+++ libft/ft_dynamic_array.c
@@ -20,12 +20,17 @@ t_dymamic_array    ft_init_dynamic_array(size_t length, size_t data_t_size)
     return (da);
 }
 
-void ft_free_dynamic_array(t_dymamic_array da)
+int ft_free_darray(t_dymamic_array *da)
 {
     size_t i;
+
     i = -1;
-   while(++i < da.capacity/da.item_size)
-        free(((uint8_t *)da.tab + (i * da.item_size)));
+    while(++i < da->capacity / da->length)
+        free((uint8_t *)da->tab + i * da->item_size);
+    ft_memdel(&da->tab);
+    if(da->tab)
+        return(0);
+    return (1);
 }
 
 uint8_t			ft_darray_grow(t_dymamic_array *da)
@@ -38,18 +43,24 @@ uint8_t			ft_darray_grow(t_dymamic_array *da)
 	if (!ptr)
 		return (0);
 	if (da->tab)
-		ft_memcpy(ptr, da->tab, da->capacity);
-	free(da->tab);
-	ft_bzero(((uint8_t *)ptr) + (da->capacity / da->item_size),
-			da->item_size * (cap - da->capacity));
-	da->tab = ptr;
-	da->capacity = cap;
-	return (1);
+    {
+      size_t i;
+
+      i = -1;
+      while(++i < (da->capacity / da->item_size))
+        memcpy((((uint8_t*)ptr) + i * da->item_size), (((uint8_t*)da->tab) + i * da->item_size), da->item_size);
+    }
+	if(ft_free_darray(da))
+    {
+        da->tab = ptr;
+        da->capacity = cap;
+    }
+    return (1);
 }
 
 void ft_add_to_darray(void *data, t_dymamic_array *da)
 {
-    if(da->length  ==  da->capacity / da->length)
+    if(da->length  ==  da->capacity / da->item_size)
         ft_darray_grow(da);
     uint8_t     i;
     memcpy(((uint8_t*)da->tab) + da->length * da->item_size, data, da->item_size);
@@ -61,12 +72,15 @@ int main(void)
     t_dymamic_array T;
     char *k = "lamine";
     char *m = "test";
-    T = ft_init_dynamic_array(16, sizeof(char *));
+    T = ft_init_dynamic_array(2, sizeof(char *));
     ft_add_to_darray(&k, &T);
     ft_add_to_darray(&m, &T);
     printf("t[0] = %s\n", ((char**)T.tab)[0]);
     printf("t[1] = %s\n", *((char**)T.tab + 1));
-    ft_free_dynamic_array(T);
+    ft_add_to_darray(&"Mohamed", &T);
+    printf("t[0] = %s\n", ((char**)T.tab)[0]);
     printf("t[1] = %s\n", *((char**)T.tab + 1));
+    printf("t[1] = %s\n", *((char**)T.tab + 2));
+    
     return (0);
 }
